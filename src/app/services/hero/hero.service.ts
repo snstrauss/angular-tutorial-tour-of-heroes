a50951f8b1a3f57dac3e9e99a01a9e3ce86400b8
snstrauss <zahavi.guy.85@gmail.com>
2020-08-03 14:53:10 +0300

    created the hero service

---
+++ src/app/services/hero/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HEROES } from '../../mock/heroes-mock';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private http: HttpClient) { }
+
+  getHeroes(): Observable<any> {
+
+    debugger;
+    return this.http.get('https://jsonplaceholder.typicode.com/users');
+  }
+}
